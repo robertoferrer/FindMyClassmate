8d9432e90ccec8b4542a73f48bff44f1f2788357
Roberto Ferrer <roberto_rfb@hotmail.com>
2015-11-21 15:10:16 -0600

    Added jquery and modified links in header

--- header.php
+++ header.php
@@ -8,6 +8,10 @@
 
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
