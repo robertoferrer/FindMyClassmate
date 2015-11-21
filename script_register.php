b0addbac2b8aaf7697f1cbd648fc676a1c992af9
Fromalaska49 <fromalaska49@gmail.com>
2015-11-21 16:07:57 -0600

    I changed it

--- script_register.php
+++ script_register.php
@@ -19,7 +19,7 @@ if(isset($_POST['fname'])&&isset($_POST['lname'])&&isset($_POST['password'])&&is
         $sanitized_lname = mysql_real_escape_string($lname);
         $sanitized_email = mysql_real_escape_string($email);
         $sanitized_phone = (int) $phone;
-        $sanitized_fname = mysql_real_escape_string($fname);
+        $sanitized_pw_hash = mysql_real_escape_string($pw_hash);
         $sql = "INSERT INTO 'users' ('fname','lname','pw_hash','email','phone') VALUES ('$sanitized_fname','$sanitized_lname','$sanitized_pw_hash','$sanitized_email','$sanitized_phone')";
         mysql_query($sql);
     }
