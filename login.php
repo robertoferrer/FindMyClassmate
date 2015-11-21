1421e5d351c6f24ab0f5e438f53923452ad439c1
Fromalaska49 <fromalaska49@gmail.com>
2015-11-21 15:13:36 -0600

    I changed it

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrew
+ * Date: 11/21/15
+ * Time: 3:06 PM
+ */
+
+?>
---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrew
+ * Date: 11/21/15
+ * Time: 3:08 PM
+ */
+include('header.php');
+
+
+
+
+?>
+<form action="script_register.php" method="POST">
+    <input type="">
+</form>
