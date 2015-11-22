2c2403ba1325db01734c6142d41a6be8d70c2013
Fromalaska49 <fromalaska49@gmail.com>
2015-11-22 08:35:50 -0600

    fixing

--- script_login.php
+++ script_login.php
@@ -16,22 +16,27 @@ mysql_select_db(ramen);
 if(isset($_POST['password'])&&isset($_POST['email'])){
     $password = $_POST['password'];
     $email = $_POST['email'];
-    $pw_hash = password_hash($password, PASSWORD_BCRYPT);
-    $sanitized_pw_hash = mysql_real_escape_string($pw_hash);
     $sanitized_email = mysql_real_escape_string($email);
-    $sql = "INSERT INTO 'users' ('fname','lname','pw_hash','email','phone') VALUES ('$sanitized_fname','$sanitized_lname','$sanitized_pw_hash','$sanitized_email','$sanitized_phone')";
+    $sql = "SELECT * FROM WHERE `email`='$sanitized_email' AND `pw_has`='$sanitized_pw_hash')";
     if(mysql_query($sql)) {
-        $result = mysql_query("SELECT * FROM 'users' WHERE 'email' = '$sanitized_email' AND 'pw_hash' = '$sanitized_pw_hash'");
+        $result = mysql_query("SELECT * FROM 'users' WHERE 'email' = '$sanitized_email' LIMIT 1");
         $row = mysql_fetch_array($result);
-        $_SESSION['fname'] = $row['fname'];
-        $_SESSION['lname'] = $row['lname'];
-        $_SESSION['email'] = $row['email'];
-        if ($row['phone'] > 0) {
-            $_SESSION['phone'] = $row['phone'];
+        $db_password = $row['pw_hash'];
+        if (password_verify($password, $db_password)) {
         }
-        $_SESSION['uid'] = $row['uid'];
-        $redirect = 'home.php';
-        header('Location: ' . $redirect);
+            $_SESSION['fname'] = $row['fname'];
+            $_SESSION['lname'] = $row['lname'];
+            $_SESSION['email'] = $row['email'];
+            if ($row['phone'] > 0) {
+                $_SESSION['phone'] = $row['phone'];
+            }
+            $_SESSION['uid'] = $row['uid'];
+            $redirect = 'home.php';
+            header('Location: ' . $redirect);
+        }
+        else {
+            die('passwords don\'t match');
+        }
     }
 }
 else {
