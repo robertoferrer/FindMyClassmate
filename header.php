2d500915b96fc472be5b88065f47395d4e2dcf2a
Fromalaska49 <fromalaska49@gmail.com>
2015-11-21 19:00:45 -0600

    Adding input styles and classes

--- header.php
+++ header.php
@@ -24,7 +24,10 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
+    <?php
+    include('head_includes.php');
+    ?>
+    ?>
 <!--    FONTS-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 </head>
