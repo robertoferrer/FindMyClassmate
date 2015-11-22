769e825553a7c90440218763f876302e529e4e30
Fromalaska49 <fromalaska49@gmail.com>
2015-11-22 01:22:15 -0600

    testing...

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrew
+ * Date: 11/22/15
+ * Time: 1:19 AM
+ */
+
+require('script_db_connect.php');
+
+$result = mysqli_query($db_link, "SELECT * FROM 'courses' LIMIT 30");
+while($row = mysqli_fetch_array($result)){
+    var_dump($row);
+    echo('<br /><br />');
+}
+?>
