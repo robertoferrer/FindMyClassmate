35ddfa0f1324792756e7dcf54af67c693b22012c
Fromalaska49 <fromalaska49@gmail.com>
2015-11-21 19:01:16 -0600

    Adding input styles and classes

--- header.php
+++ header.php
@@ -27,7 +27,6 @@
     <?php
     include('head_includes.php');
     ?>
-    ?>
 <!--    FONTS-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 </head>
