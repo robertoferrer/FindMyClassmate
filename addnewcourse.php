259ad05836f94da9cd8ae1ae7525fd9b37f5d9fe
Fromalaska49 <fromalaska49@gmail.com>
2015-11-22 09:45:32 -0600

    fixing stuff

--- addnewcourse.php
+++ addnewcourse.php
@@ -26,6 +26,12 @@ include "header.php";
                         }
                         ?>
                     </option>
+                    <?php
+                    $result = mysql_query($sql = "SELECT DISTINCT `department` FROM `courses`;");
+                    while($row = mysql_fetch_arrray($result)){
+                        echo('<option>'.htmlentities($row['description']).'</option>');
+                    }
+                    ?>
                 </select>
             </form>
             <div class="">
