99ef1efccbbd8390a18c1b7c55385164e63ea4e3
Roberto Ferrer <roberto_rfb@hotmail.com>
2015-11-21 18:50:03 -0600

    login and styles

--- header.php
+++ header.php
@@ -44,7 +44,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <div class="container">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
+                    <li ><a href="#">Home</a></li>
                     <li><a href="myCourses.php">My Courses</a></li>
                     <li><a href="#">Calendar</a></li>
                     <li><a href="#">Friends</a></li>
---
+++ myCourses.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robertoferrerb
+ * Date: 11/21/15
+ * Time: 6:38 PM
+ */
+include "header.php";
+
+?>
+    <div class="container">
+
+        <h1>My courses</h1>
+
+    </div>
+
+<?php
+
+include "footer.php";
