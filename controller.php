75c30fc8d3c76e2087b0ab12b13db4176bb77802
Roberto Ferrer <roberto_rfb@hotmail.com>
2015-11-22 05:51:00 -0600

    5 am

---
+++ controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robertoferrerb
+ * Date: 11/22/15
+ * Time: 5:40 AM
+ */
+
+include_once "functions.php";
+$function = $_POST['function'];
+
+switch($function){
+    case "getCourses":
+        return getCourses();
+        break;
+}
