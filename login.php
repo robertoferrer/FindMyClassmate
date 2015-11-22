354333bb181fe7b284cc23f7b4fca8db6c83db4c
Fromalaska49 <fromalaska49@gmail.com>
2015-11-22 08:15:24 -0600

    updating sing up link

--- login.php
+++ login.php
@@ -43,7 +43,7 @@ if($_SERVER['REQUEST_METHOD'] === "POST"){
                 <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-10">
                         <button type="submit" class="btn btn-default">Sign in</button>
-                        <a href="registration.php" style="float:right;">Sign up</a>
+                        <a href="index.php" style="float:right;">Sign up</a>
                     </div>
                 </div>
             </form>
